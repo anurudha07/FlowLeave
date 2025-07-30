196d99772841713ce953eeb79651cfbe694fbcbe
Anurudha Sarkar <132932624+anurudha07@users.noreply.github.com>
2025-07-30 22:36:17 +0530

    Update leaveThunks.js

--- frontend/src/features/leave/leaveThunks.js
+++ frontend/src/features/leave/leaveThunks.js
@@ -1,14 +1,14 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const BASE_URL = 'https://myapp-backend.onrender.com'; // ⬅️ Add this line
+const BASE_URL = 'https://flowleave-backend.onrender.com/'; 
 
 export const registerUser = createAsyncThunk('user/register', async (data) => {
-  const res = await axios.post(`${BASE_URL}/api/users/register`, data); // ⬅️ Update
+  const res = await axios.post(`${BASE_URL}/api/users/register`, data); 
   return res.data;
 });
 
 export const loginUser = createAsyncThunk('user/login', async (data) => {
-  const res = await axios.post(`${BASE_URL}/api/users/login`, data); // ⬅️ Update
+  const res = await axios.post(`${BASE_URL}/api/users/login`, data); 
   return res.data;
 });
