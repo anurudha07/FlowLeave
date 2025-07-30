dae4fafa4456dd9fa548b434e585d68f32ab2df5
Anurudha Sarkar <132932624+anurudha07@users.noreply.github.com>
2025-07-30 22:16:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
 import leaveRoutes from './routes/leaveRoutes.js';
 import { errorHandler } from './middleware/errorHandler.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import dotenv from 'dotenv';
 
 dotenv.config();
 connectDB();
@@ -18,18 +13,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Serve static frontend
-app.use(express.static(path.join(__dirname, 'public')));
-
-
 app.use('/', userRoutes);
 app.use('/', leaveRoutes);
 app.use(errorHandler);
 
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
+app.listen(port, () => console.log(`Server running on port: http://localhost:${port}`));
