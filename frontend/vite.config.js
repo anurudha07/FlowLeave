d5d60083654701020aa07a32d30441f5106e858b
Anurudha Sarkar <132932624+anurudha07@users.noreply.github.com>
2025-07-30 22:17:29 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server:{
     host: '0.0.0.0',
     port: 5173
-  },
-  build: {
-    outDir: path.resolve(__dirname, '../backend/public'),
-    emptyOutDir: true,
-  },
+  }
 })
