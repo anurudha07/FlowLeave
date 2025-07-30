e9e5521e0f624fae7867ac362e65f1757104f8bb
Anurudha Sarkar <132932624+anurudha07@users.noreply.github.com>
2025-07-30 21:46:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,35 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
 import leaveRoutes from './routes/leaveRoutes.js';
 import { errorHandler } from './middleware/errorHandler.js';
-import dotenv from 'dotenv';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-    // origin: '',
-    // methods: ["GET", "POST"],
-    // credentials: true
-}));
+app.use(cors());
 
-app.use('/', userRoutes);
-app.use('/', leaveRoutes);
+// Serve static frontend
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/api/users', userRoutes);
+app.use('/api/leave', leaveRoutes);
 app.use(errorHandler);
 
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server running on port: http://localhost:${port}`));
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
